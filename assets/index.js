319f8dafd35cae227b4d48f1df95f2aeef56df5b
Hemachandran SS <hemachandran@colanonline.com>
2026-08-26 19:09:10 +0530

    good at science and engineering

--- assets/index.js
+++ assets/index.js
@@ -139,7 +139,8 @@ const visualBooks = [
     'https://hemachandranss.github.io/books2/books/visual-books/DK_Smithsonian_Science_33__Science_as_You_39_ve_Neve',
     'https://hemachandranss.github.io/books2/books/visual-books/DK_Children_-_The_Elements',
     'https://hemachandranss.github.io/books2/books/visual-books/DK_Publishing_-_Complete_Flags_of_the_World_5th_edition_Smithsonian_Handbooks_-_2008',
-    'https://hemachandranss.github.io/books3/books/visual-books/DK_-_First_How_Things_Work_Encyclopedia'
+    'https://hemachandranss.github.io/books3/books/visual-books/DK_-_First_How_Things_Work_Encyclopedia',
+    'https://hemachandranss.github.io/books3/books/visual-books/DK_How_to_be_good_at_science_technology_engineering'
 ];
 
 const normalizeBookPath = (value) => {
