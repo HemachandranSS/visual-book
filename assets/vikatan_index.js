bb713426c2d469d464c9ef977f6bf6dc9f47cafa
Hemachandran SS <hemachandran@colanonline.com>
2026-09-13 22:27:51 +0530

    Vikatan anandha and pasumai

--- assets/vikatan_index.js
+++ assets/vikatan_index.js
@@ -1,6 +1,9 @@
 const vikatanBooks = [
   "https://hemachandranss.github.io/books10/books/visual-books/நாணயம்_விகடன்_Sep_04_2026.pdf",
-  "https://hemachandranss.github.io/books10/books/visual-books/நாணயம்_விகடன்_Sep_11_2026.pdf"
+  "https://hemachandranss.github.io/books10/books/visual-books/நாணயம்_விகடன்_Sep_11_2026.pdf",
+  'https://hemachandranss.github.io/books10/books/visual-books/ஆனந்த_விகடன்_Sep_10_2025.pdf',
+  'https://hemachandranss.github.io/books10/books/visual-books/பசுமை_விகடன்_Sep_11_2025.pdf'
+
 ];
 
 const normalizeBookPath = (value) => {
