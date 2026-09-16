0909be6e9e6b4c0f9c712f76d7de3ddbd375cc11
Hemachandran SS <hemachandran@colanonline.com>
2026-09-16 15:28:04 +0530

    Added Aval and Motor

--- assets/vikatan_index.js
+++ assets/vikatan_index.js
@@ -5,7 +5,9 @@ const vikatanBooks = [
   'https://hemachandranss.github.io/books10/books/visual-books/பசுமை_விகடன்_Sep_11_2025.pdf',
   'https://hemachandranss.github.io/books10/books/visual-books/Vikatan_Plus_Sep_11_2025.pdf',
   'https://hemachandranss.github.io/books10/books/visual-books/சக்தி_விகடன்_Sep_15_2026.pdf',
-  'https://hemachandranss.github.io/books10/books/visual-books/ஜூனியர்_விகடன்_Sep_16_2025'
+  'https://hemachandranss.github.io/books10/books/visual-books/ஜூனியர்_விகடன்_Sep_16_2025.pdf',
+  'https://hemachandranss.github.io/books10/books/visual-books/மோட்டார்_விகடன்_Sep_01_2026.pdf',
+  'https://hemachandranss.github.io/books10/books/visual-books/அவள்_விகடன்_Sep_10_2026.pdf'
 
 ];
 
