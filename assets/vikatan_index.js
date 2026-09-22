4289ada551401be7cfdcf7871ae7ae8de4cceff3
Hemachandran SS <hemachandran@colanonline.com>
2026-09-22 14:00:30 +0530

    Aval Vik sep 22

--- assets/vikatan_index.js
+++ assets/vikatan_index.js
@@ -11,7 +11,8 @@ const vikatanBooks = [
     'https://hemachandranss.github.io/books11/books/visual-books/ஆனந்த_விகடன்_Sep_17_2026.pdf',
     'https://hemachandranss.github.io/books11/books/visual-books/நாணயம்_விகடன்_Sep_18_2026.pdf',
     'https://hemachandranss.github.io/books11/books/visual-books/ஜூனியர்_விகடன்_Sep_19_2026.pdf',
-    'https://hemachandranss.github.io/books16/books/visual-books/Vikatan_Plus_Sep_21_2026.pdf'
+    'https://hemachandranss.github.io/books16/books/visual-books/Vikatan_Plus_Sep_21_2026.pdf',
+    'https://hemachandranss.github.io/books16/books/visual-books/அவள்_விகடன்_Sep_22_2026.pdf'
 
 ];
 
